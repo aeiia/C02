f27f4484408d76af1eeca07a1e7ef332a8d5e25f
tiida <takehi.iida@gmail.com>
2025-10-23 19:30:05 +0900

    ex09 ft_strcapitalize.c

---
+++ ex09/ft_strcapitalize.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiida <tiida@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 18:38:28 by tiida             #+#    #+#             */
+/*   Updated: 2025/10/23 19:27:33 by tiida            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] = str[i] - 32;
+		else if (i > 0 && !((str[i - 1] >= '0' && str[i - 1] <= '9')
+				|| (str[i - 1] >= 'a' && str[i - 1] <= 'z')
+				|| (str[i - 1] >= 'A' && str[i - 1] <= 'Z')))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
+/*
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] = str[i] - 32;
+		if (!((str[i - 1] >= 'a' && str[i - 1] <= 'z')
+				|| (str[i - 1] >= 'A' && str[i - 1] <= 'Z')
+				|| (str[i - 1] >= '0' && str[i - 1] <= '9')))
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
+*/
+
+/* Capitalize the very first letter of the string...
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[0] = str[0] - 32;
+		if (i > 0 && (str[i] >= 'A' && str[i] <= 'Z'))
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
+*/
